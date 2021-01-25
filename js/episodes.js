5c3ba4692c2765bd0af1f36100c636a81fd35dff
Anntroy <ana.troyan@gmail.com>
2021-01-25 13:07:50 +0100

    Adding character container

---
+++ js/episodes.js
@@ -0,0 +1,26 @@
+
+const fetchEpisodes = () => {
+    axios.get("https://rickandmortyapi.com/api/episode")
+    .then((response) => {
+        displayEpisodes(response);
+        // displayEpisodesMain(response)
+    })
+    .catch(error => console.error(error));
+
+};
+
+fetchEpisodes();
+
+const displayEpisodes = (response) => {
+    console.log(response)
+
+	const htmlString = response.data.results
+		.map((episode) => {
+            return `
+                <li class="episode" id="${episode.id}">
+                <p>Episode ${episode.id}: ${episode.name}</p>
+                </li>`;
+		})
+        .join('');
+        episodesList.innerHTML = htmlString;
+};
--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
-const fetchCharacters = () => {
-    axios.get("https://rickandmortyapi.com/api/episode")
-    .then((response) => {console.log(response.data.results[0].characters)})
-    .catch(error => console.error(error));
+// const fetchCharacters = () => {
+//     axios.get("https://rickandmortyapi.com/api/episode")
+//     .then((response) => displayEpisodesMain(response))
+//     // .then((response) => {console.log(response.data.results[0].characters)})
+//     .catch(error => console.error(error));
 
+// };
+
+// fetchCharacters();
+const makeCharacter = (cid, cname, cimage, cstatus, cspecies) => ({cid, cname, cimage, cstatus, cspecies});
+let charactersArray = [];
+
+const displayEpisodesMain = (response) => {
+
+    section.innerHTML = `
+        <h2 class="header__button-h2">${response.data.results[0].name}</h2>
+        <p class="header__button-p">${response.data.results[0].air_date}</p>
+        <article class="section__article" id="sectionArticle"></article>`;
+    const htmlString = response.data.results[0].characters
+    .forEach((characters) => {
+            axios.get(`${characters}`)
+            .then((characters) => getCharacter(characters))
+            .catch(error => console.error(error));
+		})
+        // .join('');
+    // sectionArticle.innerHTML = `
 };
 
-fetchCharacters();
+const getCharacter = (characters) => {
 
-const fetchEpisodes = () => {
-    axios.get("https://rickandmortyapi.com/api/episode")
-    .then((response) => {console.log(response)})
-    .catch(error => console.error(error));
-
-};
-
-fetchEpisodes();
+    console.log(characters.data)
+    const character = makeCharacter(characters.data.id, characters.data.name, characters.data.image, characters.data.status, characters.data.species);
+    // article.innerHTML += `
+    // <figure class="section__article-figure ${element.keyword} " style="background-image: url('${element.src}');"></figure>`
+    // 
+    // const charactersString = 
+    charactersArray.push(character);
+    console.log(charactersArray)
+}
